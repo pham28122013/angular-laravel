8d3899a89f0fe74cce9631d953150f736a5f924d
pham28122013 <15i1nguyendipham@gmail.com>
2020-01-08 12:26:04 +0700

    change

--- app/users/users.controller.js
+++ app/users/users.controller.js
@@ -51,42 +51,71 @@ angular
 
   self.onCreateAnUserClick = function () {
     self.userForm.status = true;
-    self.userForm.edit = false;
+    // self.userForm.edit = false;
     self.userForm.name = '';
     self.userForm.email = '';
     self.userForm.password = '';
   }
    
   self.onEditButtonClick = function (user) {
-    self.userForm.status = true;
     self.userForm.edit = true;
     self.userForm.editUserID = user.id;
     self.userForm.name = user.name;
     self.userForm.email = user.email;
     self.userForm.password = user.password;
   };
+
+
+
+
   self.onshow = function (user) {
-   
-    
-      self.userForm.status = true;
-    self.userForm.show = true;
-    self.userForm.editUserID = user.id;
-    self.userForm.name = user.name;
-    self.userForm.email = user.email;
-    self.userForm.password = user.password;
-    
+    // console.log(id);
+  //   $http.get('http://localhost:82/public/api/users/' + id).then(function(response){
+  //     // console.log(response);
+  //     $http
+  //     .get('http://localhost:82/public/api/users')
+  //     .then(function (response) {
+  //       self.users = response.data;
+  //     });
+  // })  
+  self.userForm.show = true;
+  self.userForm.editUserID = user.id;
+  self.userForm.name = user.name;
+  self.userForm.email = user.email;
+  self.userForm.password = user.password;
+  };
+
+
+
+  self.onAddUserFormClick = function () {
+    console.log(self.userForm);
+    // if (self.userForm.edit) {
+      $http
+      .post('http://localhost:82/public/api/users', self.userForm ).then(function (response) {
+         console.log(response);  
+        // if (parseInt(response.data.status) === 200) {
+        //   self.userForm.status = false;
+        //   var user = response.data.user
+        // }
+         
+       });
+      
   };
   
-    // self.onGetAJAX = function () {
-    //   $http
-    //     .get('http://localhost:81/public/api/users')
-    //     .then(function (response) {
-    //       //  console.log('aaa');
-    //       self.users = response.data.users;
-
-    //     });
-    // }
-
+  self.onUpdateUserFormClick = function (id){
+    // console.log(self.userForm);
+    // if (self.userForm.edit) {
+      $http
+      .put('http://localhost:82/public/api/users/' + id, self.userForm ).then(function (response) {
+         console.log(response);  
+        if (parseInt(response.data.status) === 200) {
+          self.userForm.status = true;
+          // var user = response.data.user
+        }
+        self.users = response.data.status;
+       });
+      return;
+  };
    
 
   }]);
